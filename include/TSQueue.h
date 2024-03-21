3f74930e778b946c9b1960e1db1d83e36cf59c38
Frank Earl <frank@earlconsult.com>
2024-03-21 17:22:49 -0500

    Add TSQueue.h and serial.hpp to the mix...

---
+++ include/TSQueue.h
@@ -0,0 +1,36 @@
+#include <thread>
+#include <queue>
+
+#pragma once
+
+// Implement a fairly proper threadsafe queue...
+template <typename T> class TSQueue {
+    public:
+        void push(const T& item) 
+        {
+            {
+                std::lock_guard lock(mutex);
+                queue.push(item);
+            }
+
+            cond_var.notify_one();
+        };
+
+        T& front() 
+        {
+            std::unique_lock lock(mutex);
+            cond_var.wait(lock, [&]{ return !queue.empty(); });
+            return queue.front();
+        };
+
+        void pop() 
+        {
+            std::lock_guard lock(mutex);
+            queue.pop();
+        };
+
+    private: 
+        std::mutex mutex;
+        std::condition_variable cond_var;
+        std::queue<T> queue;
+};
